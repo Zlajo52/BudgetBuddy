15de36140bae9956637c12ebefa6f173b12357d7
zlajo52 <zlatkobokor52@gmail.com>
2026-03-09 11:05:34 +0100

    pocetno postavljanje projekta, spajanje sa firebaseom

---
+++ BudgetBuddy/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <p class="flex h-screen items-center justify-center bg-base-200">
+      <button class="btn btn-primary">Hello tailwind!</button>
+    </p>
+  );
+};
+
+export default App;
